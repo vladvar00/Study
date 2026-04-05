86f6ed845fd96288bf0bf8188e36537f3a861af4
vladvar00 <vladislavvaranica28@gmail.com>
2026-04-05 17:11:38 +0300

    Add files via upload

---
+++ lr_21/PresentationTier/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using LogicTier;
+
+namespace PresentationTier
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            // Путь к файлу. Для VS Code лучше указывать так, 
+            // если файл лежит в корне всей папки
+            string path = "../cars.txt";
+            DataContext = new CarManager(path);
+            InitializeComponent();
+        }
+    }
+}
